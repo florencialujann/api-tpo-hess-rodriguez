ae031ea44922b2817fe1aca1a2d768629e1c0b20
Agustin Hess <agustin_hess@yahoo.com.ar>
2023-07-07 00:55:32 -0300

    Entrega Final

--- back/server.js
+++ back/server.js
@@ -2,19 +2,22 @@ require('dotenv').config();
 const express = require('express');
 const {dbConnection} = require('./src/db/config');
 
-
 const app = express();
+const cors = require("cors");
+app.use(cors());
 dbConnection();
+
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.status(200).send({ message: "You are connected to the project" });
+  res.status(200).send({ message: "Conexion establecida" });
 });
 
-app.use("/api/productos", require("./src/routes/productos.routes"));
+//Existe una URI por cada entidad.
+app.use("/api/contactos", require("./src/routes/contactos.routes"));
 app.use("/api/usuarios", require("./src/routes/usuarios.routes"));
 
 app.listen(process.env.PORT, () =>{
